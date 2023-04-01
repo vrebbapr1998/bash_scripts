b5767fadcb45ffd563353aebf4ff3d9000571c8d
Venkat Koushik <venkatkoushik@yahoo.com>
2023-04-01 21:08:16 +0530

    Third commit

---
+++ passing_arguments.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+#The above line tells about path of intrepeter.
+#when we pass arguments $0 stores the shell file name, and the values after $0 like $1, $2, etc... store the rest arguments
+#Think like this whole execute command is the argument!😉
+
+echo "You program ${0} is currently running and this is your FIRST argument."
+echo "This is your SECOND argument ${1}."
+echo "This is your THIRD argument ${2}."
+
+
+
