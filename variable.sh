98b2ba53cf1e3a121da8655fc4a205ec40db789e
Venkat Koushik <venkatkoushik@yahoo.com>
2023-04-01 20:25:32 +0530

    Second Commit

---
+++ variable.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+#The above line tells about path of intrepeter.
+# declaring variables one, two and three
+one=Hello
+two="_World."
+three=sh
+#If the variable contains only string without any special char no double quote as the varable one
+#We use $ to avoid errors while executing.
+echo "$one$two$three"
