02ddb90bfa4b329d66bc959baf1fa63e6b07032a
Venkat Koushik <venkatkoushik@yahoo.com>
2023-04-04 19:58:05 +0530

    second of operators

--- basic_operators.sh
+++ basic_operators.sh
@@ -2,10 +2,10 @@
 #The above line tells about path of intrepeter.
 #To do basic arthemitic operations we do it like this as $((expression))
 #Please refer the code below as an example
-#The below program calculates simple intrest
+#The below program calculates simple interest
 
 principle=100
 intrest_rate=10
 loan_tenure_in_years=5
-intrest=$((${principle}*${loan_tenure_in_years}*${intrest_rate}/100))
-echo "The intrest is ₹"${intrest}
+interest=$((${principle}*${loan_tenure_in_years}*${intrest_rate}/100))
+echo "The interest is ₹"${interest}
